3b8d2c1de0762103f8fddef3de0dad7403b32e52
victordones <victorhguimaraes.vh@gmail.com>
2024-02-28 14:07:04 -0300

    1

--- server/server.js
+++ server/server.js
@@ -49,14 +49,6 @@ http
 
 app.use(cors)
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
 app.get('/', (req, res) => {
   res.status(200).json({
     msg: 'bem-vindo a nossa API'
